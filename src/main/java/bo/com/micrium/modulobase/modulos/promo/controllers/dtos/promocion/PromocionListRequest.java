950040fe96bdd2080abaec732f3c61e8cc8a2805
DaveTorrez <davidtn52@gmail.com>
2026-07-16 16:00:33 -0400

    feat: Add ListRequest DTOs for Beneficio, Promocion, Target, and Uso to enhance request handling

---
+++ src/main/java/bo/com/micrium/modulobase/modulos/promo/controllers/dtos/promocion/PromocionListRequest.java
@@ -0,0 +1,16 @@
+package bo.com.micrium.modulobase.modulos.promo.controllers.dtos.promocion;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+public class PromocionListRequest implements Serializable {
+    private String nombre;
+    private String descripcion;
+    private String fechaInicio;
+    private String fechaFin;
+    private String isActive;
+    private String limiteUso;
+    private String limitePorCliente;
+}
---
+++ src/main/java/bo/com/micrium/modulobase/modulos/promo/controllers/dtos/uso/UsoListRequest.java
@@ -0,0 +1,16 @@
+package bo.com.micrium.modulobase.modulos.promo.controllers.dtos.uso;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+@AllArgsConstructor
+@Data
+public class UsoListRequest implements Serializable {
+    private String promocionId;
+    private String clienteId;
+    private String ventaId;
+    private String cantidadDescuento;
+}
